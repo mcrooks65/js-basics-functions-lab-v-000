a57d657b4e6a7e5b8f10762a369d07d10fa4d383
Matthew Crooks <mfcrooks@gmail.com>
2018-08-12 23:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,7 @@ function calculatesFarePrice(start, destination) {
 
   if (distance <= 400) {
     return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
   }
 }
